5d43f95edd0dfef0e99d3c7c7dadda0d22867d2f
Mary West <westmarye1219@gmail.com>
2019-03-04 21:35:49 -0600

    objects

--- main.js
+++ main.js
@@ -46,3 +46,78 @@ console.log('cats'.length);
 const quote = 'winter is coming';
 console.log(quote.indexOf('is')) //index of is a function - this will find the word is and then find the index value of the y ( return 7)
 console.log(quote.indexOf('bunny')) // if it does not find it, it returns a -1 ( means did not find)
+
+// Objects
+
+//think of objects as a "bucket of cats" cats would move - objects are made of key value pairs
+// doesn't matter the order of the key value pairs
+
+const catName = () => {
+    return 'killer'
+}
+
+const bucketOCats = {
+    cat1: 'fluffy',
+    cat2: 5,
+    cat3: catName,
+    cat4: {
+        water: 'nope',
+        sunlight: 'yep'
+    }
+};
+
+// dot notation
+console.log(bucketOCats.cat2);
+
+// bracket notation
+// function['keyname']
+console.log(bucketOCats['cat2']);
+
+// how to get to the cat4 object key notatition water
+console.log(bucketOCats.cat4.water)
+
+// dot notation is preferred
+
+let hitchhikers_guide = {
+    characters: ["Zaphod", "Arthur", "Ford", "Trillian"],
+    catchphrase: "Don't Panic",
+    random_facts: {
+        copies_sold: 1400000,
+        formats: ["radio", "TV", "film", "graphic novel"],
+        ultimate_answer: {
+            meaning_of_life: 42
+        }
+    }
+}
+
+console.log('meaning_of_life: ', hitchhikers_guide.random_facts.ultimate_answer.meaning_of_life);//42
+
+let employee = {
+    name: "Jeff Winger",
+    age: 37,
+    department: "legal",
+    hire_date: "09/22/2010",
+    vacation_days: 20,
+    employee_of_the_month: false
+
+}
+
+printToDom('printing', `Our companys lawyer is  , ${employee.name}`);
+printToDom('printing', `Jeff was hired on', ${employee.hire_date}`);
+printToDom('printing',employee.vacation_days);
+printToDom('printing',employee['vacation_days']);
+printToDom('printing',employee.employee_of_the_month);
+
+console.log(`Our companys lawyer is  , ${employee.name}`);
+console.log( `Jeff was hired on , ${employee.hire_date }`);
+console.log(employee.vacation_days);
+console.log(employee['vacation_days']);
+console.log(employee["employee_of_the_month"]);
+
+// since this is in quotes it needs to be in bracket notation
+let eom = "employee_of_the_month";
+employee[eom] = false;
+
+console.log(employee);
+
+// employee of the month is the key
